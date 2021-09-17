4f8ad6f30f027c93291ebc0c64ea9f0a65452044
Elad Or <elad725@gmail.com>
2021-09-17 13:20:27 +0300

    updated fetch commands

--- front/src/api/fetch.js
+++ front/src/api/fetch.js
@@ -1,28 +1,58 @@
 export const GET = async (url, params = undefined) => {
-    try {
-        if (params) {
-            url += params.join("/"); //takes all parameters and puts a / between them
-        }
-        let res = await fetch(url, { method: 'GET' });
-        let data = await res.json();
-        return data;
-
-    } catch (error) {
-        console.error(error);
-    }
-}
+	try {
+		if (params) {
+			url += params.join("/"); //takes all parameters and puts a / between them
+		}
+		let res = await fetch(url, { method: "GET" });
+		let data = await res.json();
+		return data;
+	} catch (error) {
+		console.error(error);
+	}
+};
 
 export const POST = async (url, body = undefined) => {
-    try {
-        let res = await fetch(url, {
-            method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
-            body: body ? JSON.stringify(body) : ""
-        });
-        let data = await res.json();
-        return data;
+	try {
+		let res = await fetch(url, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: body ? JSON.stringify(body) : ""
+		});
+		let data = await res.json();
+		return data;
+	} catch (error) {
+		console.error(error);
+	}
+};
 
-    } catch (error) {
-        console.error(error);
-    }
-}
+export const PUT = async (url, params = undefined, body = undefined) => {
+	try {
+		if (params) {
+			url += params.join("/"); //takes all parameters and puts a / between them
+		}
+		let res = await fetch(url, {
+			method: "PUT",
+			headers: { "Content-Type": "application/json" },
+			body: body ? JSON.stringify(body) : ""
+		});
+		let data = await res.json();
+		return data;
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+export const DELETE = async (url, params = undefined) => {
+	try {
+		if (params) {
+			url += params.join("/"); //takes all parameters and puts a / between them
+		}
+		let res = await fetch(url, {
+			method: "DELETE",
+		});
+		let data = await res.json();
+		return data;
+	} catch (error) {
+		console.error(error);
+	}
+};
