dada498b2dc7a72727a4f0f6e9d1b700516e6173
Elado5 <elad725@gmail.com>
2021-09-22 01:42:21 +0300

    base for api functions in main.js

--- front/src/components/Main.js
+++ front/src/components/Main.js
@@ -1,9 +1,10 @@
 import React, {useEffect} from 'react'
 import {customers} from '../api/api';
-import {GET} from '../api/fetch';
+import {GET, POST, PUT, DELETE} from '../api/fetch';
 
 export default function Main() {
 
+    //* Customer
     const LoadSpecificUser = async (id) => {
         let c1 = await GET(customers.get_by_id,[id]);
         console.info(c1);
@@ -13,6 +14,84 @@ export default function Main() {
         let c = await GET(customers.get_all);
         console.table(c);
     }
+
+    const RegisterCustomer = async () => {
+    }
+
+    const LoginCustomer = async () => {
+    }
+
+    const UpdateCustomer = async () => {
+    }
+
+    const DeleteCustomer = async () => {
+    }
+
+    //* Address
+    const LoadAllAdresses = async () => {}
+    const LoadSpecificAdress = async () => {}
+    const AddAddress = async () => {}
+    const ActivateAddress = async () => {}
+    const DeactivateAddress = async () => {}
+    const DeleteAddress = async () => {}
+
+    //* Category
+    const LoadAllCategories = async () => {}
+    const LoadSpecificCategory = async () => {}
+    const AddCategory = async () => {}
+    const UploadCategoryImage = async () => {}
+    const UploadMultipleCategoryImages = async () => {}
+    const UpdateCategory = async () => {}
+    const ActivateCategory = async () => {}
+    const DeactivateCategory = async () => {}
+    const DeleteCategory = async () => {}
+
+    //* CAC
+    const LoadAllCACs = async () => {}
+    const LoadSpecificCAC = async () => {}
+    const AddCAC = async () => {}
+    const DeleteCAC = async () => {}
+
+    //* Product
+    const LoadAllProducts = async () => {}
+    const LoadSpecificProduct = async () => {}
+    const LoadProductPreview = async () => {}
+    const LoadProductPreview2 = async () => {}
+    const LoadProductDiscount = async () => {}
+    const LoadActiveProducts = async () => {}
+    const LoadInactiveProducts = async () => {}
+    const AddProduct = async () => {}
+    const UploadProductImage = async () => {}
+    const UploadMultipleProductImages = async () => {}
+    const UpdateProduct = async () => {}
+    const ChangeProductDiscount = async () => {}
+    const ChangeCategoryDiscount = async () => {}
+    const ChangeSubCategoryDiscount = async () => {}
+    const CancelAllDiscounts = async () => {}
+    const CancelProductDiscount = async () => {}
+    const CancelCategoryDiscount = async () => {}
+    const CancelSubCategoryDiscount = async () => {}
+    const ActivateProduct = async () => {}
+    const DeactivateProduct = async () => {}
+    const DeleteProduct = async () => {}
+
+    //* Shop
+    const LoadAllShops = async () => {}
+    const LoadSpecificShop = async () => {}
+    const AddShop = async () => {}
+    const ActivateShop = async () => {}
+    const DeactivateShop = async () => {}
+    const DeleteShop = async () => {}
+
+    //* Order
+    const LoadAllOrders = async () => {}
+    const LoadSpecificOrder = async () => {}
+    const AddOrder = async () => {}
+    const ActivateOrder = async () => {}
+    const DeactivateOrder = async () => {}
+    const DeleteOrder = async () => {}
+
+    //בדיקה
     useEffect(() => {
         LoadSpecificUser(1);
         LoadSpecificUser(2);
