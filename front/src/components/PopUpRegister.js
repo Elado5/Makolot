66a3818441522e8aa437cbd720850c4b0dfd8d20
Elad Or <elad725@gmail.com>
2021-10-04 21:25:15 +0300

    Update PopUpRegister.js

--- front/src/components/PopUpRegister.js
+++ front/src/components/PopUpRegister.js
@@ -1,6 +1,17 @@
 import React from 'react'
+import customers from '../api/api'
+import {POST, GET} from '../api/fetch'
 
 export default function PopUpRegister() {
+
+    const RegisterCustomer = async () => {
+        let c = {
+
+        }
+        let res = await POST(customers.post_register, {id: 1, name: "Moshe"} );
+        //* res is the response from the server
+    }
+
     return (
         <div class="popup-reg">
             
