2080d588f9ff16261ef1fc6ac34f419600a258a5
Elad Or <elad725@gmail.com>
2021-10-31 01:22:43 +0300

    sync front api
    just making sure it doesn't get lost after getting rid of ''retailers'' folder

--- front/src/api/baseAPI.js
+++ front/src/api/baseAPI.js
@@ -1,1 +1,4 @@
-export const baseAPI = "http://localhost:6000/api";
+//? Makes it easier to read in 'api.js' file and prevents rewriting of the long address.
+export const baseAPI = "http://localhost:5000/api";
+
+//? later can be changed to the actual address and will change everywhere easily.
--- front/src/api/fetch.js
+++ front/src/api/fetch.js
@@ -1,3 +1,10 @@
+//*We define the GET/POST/PUT/DELETE functions, each do the proccess of:
+//? 1. taking a url and/or params/body
+//? 2. fetching the url with the chosen method (GET/POST/PUT/DELETE) and saving it in 'res' variable
+//? 3. changing res to json format and saving in 'data' variable
+//? 4. returning the result (data) 
+
+
 export const GET = async (url, params = undefined) => {
 	try {
 		if (params) {
