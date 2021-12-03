63ffaaef167647cc3727b9d4ff93692bc19d0fd2
Elad Or <elad725@gmail.com>
2021-12-03 14:04:43 +0200

    Update AboutScreen.js

--- front/src/screens/AboutScreen.js
+++ front/src/screens/AboutScreen.js
@@ -5,8 +5,10 @@ const AboutScreen = () => {
 	return (
 		<Container>
 			<Title>הסיפור שלנו</Title>
+            <HrLine/>
 			<BigText>נעים להכיר, אנחנו עמותה חברתית כלכלית שבאה להציע שינוי בעולם הקמעונות בישראל.</BigText>
-			<Text>
+            <HrLine/>
+            <Text>
 				בים הקמעונות יש הרבה דגים. ובשנים הראשונות של מדינת ישראל, הופיעו דגים קטנים רבים ששחו בזרם הכלכלי חברתי
 				וניסו לטפח את עסקיהם ולהעניק שירות אישי ושכונתי למרחב בו השתקעו וחיו. אך בחלוף הזמן, החלו מספר דגים
 				קטנים לאכול זה את זה ומהר מהר השתנו מי הים הקמעונאי ואותם דגים גדולים ואדירים החלו מאיימים יותר ויותר על
@@ -18,6 +20,7 @@ const AboutScreen = () => {
 				חיוניותם ועוצמתם לנוע במרחבי הים הקמעונאי כגוף גדול , יצירתי וחכם על פני אלו שביקשו לנטרל ולאכול אותו.
 			</Text>
 			<BigText>הסיפור הזה הוא הסיפור שלנו!</BigText>
+            <HrLine/>
 			<Text>
 				צמחנו מעולם הקמעונאות, כבעלי מרכולים ועסקים קטנים ממש כמוכם, חווינו אתגרים עם כניסתם של שחקנים גדולים
 				ורבי השפעה לשוק הריכוזי ביותר בישראל בשנים האחרונות, עברנו התמודדויות לא פשוטות בניסיון להישאר רלוונטיים
@@ -34,6 +37,7 @@ const AboutScreen = () => {
 				החדש והטכנולוגי הבא עלינו לטובה.
 			</Text>
 			<BigText>מה הרעיון שלנו</BigText>
+            <HrLine/>
 			<Text>
 				עמותת פורום מזון ומכולת בתמיכת משרד הכלכלה והתמ"ת מציעה תוכנית פעולה מקיפה המאגדת את כלל המכולות והעסקים
 				הקטנים בישראל, מעניקה לה יתרון יחסי מעוצמת הביחד דרך שירותי מחסן לוגיסטי, מחירים אטרקטיבים, הנגשה של
@@ -45,36 +49,73 @@ const AboutScreen = () => {
 				החלשה ביותר במדינת ישראל ולהפוך אותנו לגדולים, במידות טובות ובעסקים טובים לתפארת מדינת ישראל.
 			</Text>
 			<BigText>רגע, איך אפשר לעשות את כל השינוי הזה?</BigText>
-			<Text>פשוט זה לא אבל, גם לא כל כך מסובך…נסביר את השינוי בשלושה צעדים:</Text>
-            
+            <HrLine/>
+			<Text>פשוט זה לא, אבל גם לא כל כך מסובך…נסביר את השינוי בשלושה צעדים:</Text>
+			<ol type="1">
+				<ListText>
+					נאגד את כלל המכולות והעסקים הקטנים תחת מעטפת לוגיסטית אחידה שתעניק שירותים משמעותיים בפריסה ארצית,
+					כגון: מחסן לוגיסטי, שירותי משלוחים, מחירים אטרקטיבים מספקים, יבואנים, חקלאים ויצרנים קטנים כגדולים.
+				</ListText>
+				<ListText>
+					נשלב תהליך אוטומציה בעזרת מערכות מחשוב מתקדמות שיסייעו לנו לקדם את כלל העסקים הקטנים והמכולות וליישר
+					קו עם הקמעונות המודרנית של העידן החדש.
+				</ListText>
+				<ListText>
+					ננהל מאגר נזקקים ונפעל לאגד את גיוס התרומות בכדי להשיב לקהילה ולחזק את השכבות החלשות דרך ביטחון
+					תזונתי משמעותי.
+				</ListText>
+			</ol>
+			<BigText>נשמע לי מעולה, איך אוכל להצטרף אליכם?</BigText>
+            <HrLine/>
+			<Text>
+				שלוש דרכים קלות ופשוטות להצטרף אלינו עוד היום ולקחת חלק במיזם הכלכלי חברתי שעתיד לשנות את פני הים
+				הקמעונאי בו אנו שוחים כיום:
+			</Text>
+            <HrLineSmall/>
+			<ol type="1">
+				<ListText>
+                    מוזמנים להכנס לאתר עמותת פורום מזון ומכולת- makolot.org.il
+                    <br/>
+                    להכנס לדף המצטרפים , להשאיר פרטים ונשמח ליצור עימכם קשר בהקדם</ListText>
+				<ListText>ניתן לשלוח מייל לאימייל של העמותה בכתובת: elistavi@gmail.com</ListText>
+				<ListText>ניתן ליצור קשר טלפוני עם העמותה דרך הסלולרי : 055-6663999</ListText>
+				<ListText>או לשלוח הודעה לוואטס אפ של העמותה בטלפון: 055-6663999</ListText>
+			</ol>
+            <HrLine/>
+			<BigText>
+				*אתם עדין עוד חושבים על זה?, אל תחשבו כמו דג קטן, אתם הרבה יותר גדולים ממה שנדמה לכם! בואו לקחת חלק
+				במהפכה הקמעונאית האדירה שניצור יחדיו בים האפשרויות הגדול של הכלכלה והחברה בישראל.
+			</BigText>
+            <HrLine/>
 		</Container>
 	);
 };
 
 const Container = styled.div`
 	 {
-        display: flex;
-        flex-direction: column;
-        direction: rtl;
-		background-color: #fafafa;
-		z-index: 1;
 		display: flex;
-		justify-content: center;
-		align-items: center;
+		flex-direction: column;
+        align-items: center;
+		direction: rtl;
+		background-color: #fafafa;
+		z-index: 3;
 		height: 100vh;
 		position: fixed;
 		top: 0;
 		bottom: 0;
 		right: 0;
 		left: 0;
+        overflow-y:scroll;
+        overflow-x:hidden;
 	}
 `;
 
 const Title = styled.div`
 	 {
 		font-size: 3rem;
+        color: navy;
 		font-weight: bold;
-		box-shadow: 0px 0px 3px black;
+		text-shadow: 0px 0px 3px black;
 	}
 `;
 
@@ -82,14 +123,31 @@ const BigText = styled.div`
 	 {
 		font-size: 2rem;
 		color: navy;
-		box-shadow: 0px 0px 1px black;
-	}
+		text-shadow: 0px 0px 1px black;
+        text-align: center;
+
+    }
 `;
 
-const Text = styled.div`
+const Text = styled.span`
 	 {
 		font-size: 1rem;
+        text-align: right;
 	}
 `;
 
+const ListText = styled.li`
+	 {
+		font-size: 1rem;
+        text-shadow: 0px 0px 1px black;
+        text-align: right;
+	}
+`;
+
+const HrLine = styled.hr`{
+    width: 85%;
+}`
+const HrLineSmall = styled.hr`{
+    width: 45%;
+}`
 export default AboutScreen;
